493ece6363e75eb107e8ea7aab93433fe2450a6b
abrotor <abrotor@uoc.edu>
2026-01-04 14:11:04 +0100

    x

--- util/file_manager.py
+++ util/file_manager.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from products import *
 
 
 class CSVFileManager:
@@ -6,9 +7,13 @@ class CSVFileManager:
     self.path = path
   def read(self) -> str:
     return pd.read_csv(self.path)  
-  def write(dataFrame):
+  def write(product_list):
     with open('llista_info.txt','w') as f: 
-      f.write('Hola, mundo!')
+      f.write("List of", str(product_list))
+      f.write("\n")
+      for z in product_list:
+        f.write(Product.describe(z))
+        f.write("\n")
 
 
 pass
